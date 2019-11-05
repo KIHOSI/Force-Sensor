c57452a535c042285b0673bb2afa65bbce284fb2
KIHOSI <amie12349@gmail.com>
2019-11-05 15:04:21 +0800

    Init commit

---
+++ Code/Force_Sensor/HX711-Load-Cell-Amplifier/HX711-Load-Cell-Amplifier.ino
@@ -0,0 +1,54 @@
+/*
+  Example using the SparkFun HX711 breakout board with a scale
+  By: Nathan Seidle
+  SparkFun Electronics
+  Date: November 19th, 2014
+  License: This code is public domain but you buy me a beer if you use this and we meet someday (Beerware license).
+
+  This example demonstrates basic scale output. See the calibration sketch to get the calibration_factor for your
+  specific load cell setup.
+
+  This example code uses bogde's excellent library: https://github.com/bogde/HX711
+  bogde's library is released under a GNU GENERAL PUBLIC LICENSE
+
+  The HX711 does one thing well: read load cells. The breakout board is compatible with any wheat-stone bridge
+  based load cell which should allow a user to measure everything from a few grams to tens of tons.
+
+  Arduino pin 2 -> HX711 CLK
+  3 -> DAT
+  5V -> VCC
+  GND -> GND
+
+  The HX711 board can be powered from 2.7V to 5V so the Arduino 5V power should be fine.
+
+*/
+
+#include "HX711.h"
+
+//#define calibration_factor -7050.0 //This value is obtained using the SparkFun_HX711_Calibration sketch
+#define calibration_factor -98680 //TAL220, 10kg 1:-98300, 2:98480
+
+#define DOUT  3
+#define CLK  2
+
+HX711 scale(DOUT, CLK);
+
+void setup() {
+  Serial.begin(57600);
+  //Serial.println("HX711 scale demo");
+
+  scale.set_scale(-98680); //This value is obtained by using the SparkFun_HX711_Calibration sketch
+  scale.tare();  //Assuming there is no weight on the scale at start up, reset the scale to 0
+
+  //Serial.println("Readings:");
+}
+
+void loop() {
+  //Serial.print("Reading: ");
+  float weight = scale.get_units() * 0.453592 * 9.8;
+  //Serial.print(scale.get_units() * 0.453592, 2); //scale.get_units() returns a float
+  Serial.println(weight);
+  //Serial.print(" kgs"); //You can change this to kg but you'll need to refactor the calibration_factor
+  //Serial.println();
+  delay(100);
+}
